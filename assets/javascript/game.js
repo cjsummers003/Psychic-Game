4e0094a2833eb83bc73a96b33a1f093a033e65bb
cj summers <cjsummers003@hotmail.com>
2018-12-05 12:59:42 -0500

    psychic game

---
+++ assets/javascript/game.js
@@ -0,0 +1,43 @@
+var computerChoice = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",]
+
+var wins = 0;
+var losses = 0;
+var numGuesses = 9;
+var guessChoices = [];
+
+document.onkeyup = function (event) {
+    userGuess = event.key;
+
+    var computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];
+    var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+
+    if (options.indexOf(userGuess) > -1) {
+
+        if (userGuess === computerGuess) {
+            wins++;
+            numGuesses = 9;
+            guessChoices = [];
+        }
+
+        if (userGuess != computerGuess) {
+            numGuesses--;
+            guessChoices.push(userGuess);
+        }
+
+        if (numGuesses === 0) {
+
+            numGuesses = 9;
+            losses++;
+            guessChoices = [];
+        }
+    }
+
+    var newhtml =
+        "<p>Guess what letter I'm thinking of!</p>" +
+        "<p>Wins: " + wins + "</p>" +
+        "<p>Losses: " + losses + "</p>" +
+        "<p>Guesses Left: " + numGuesses + "</p>" +
+        "<p>Your Guesses so far: " + guessChoices.join(", ") + "</p>";
+
+    document.querySelector("#game").innerHTML = newhtml;
+}
